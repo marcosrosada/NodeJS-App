4ce07da5cc4503fe8b3a07dc45863b9c3e467c37
Marcos Rosada <ochild@gmail.com>
2016-01-10 19:52:31 -0200

    First commit
    Added some features about route and log

--- index.js
+++ index.js
@@ -11,7 +11,31 @@ var fs      = require("fs"),
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
   
-  res.end("Criando Web App");
+  var logTxt = fs.createWriteStream(pathFiles + "log.txt", { flags: "a"});
+  logTxt.write(date + ": Acesso a página " + req.url + "\n");
+
+  if (req.url == "/")
+  {
+      readPage("index.html");
+  }
+  else if (req.url == "/usuario")
+  {
+      readPage("usuario.html");
+  }
+  else
+  {
+      message = "Página não encotrada";
+  }
+
+  
+  
+  function readPage(pageFile)
+  {
+      fs.readFile(pathView + pageFile, function(e, html) {
+          if (e) message = "Página " + pageFile + " não encotrada";
+          res.end(html);
+      });
+  }
 
 }).listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
